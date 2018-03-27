51a8bb67f9824e3e19967d2b768074b8d3150ce8
Kunal Deshmukh <kunaldeshmukh27@gmail.com>
2018-03-27 16:52:51 -0700

    trying to click on that link !

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 document.body.style.border = "5px solid red";
+// trying to click on that window:
 document.getElementById("customer_review-").style.border = "thin dotted red";
+document.getElementById("see-all-reviews-link-foot").style.border = "thin dotted green"; 
+var href = $("#data-hook").attr("href");
