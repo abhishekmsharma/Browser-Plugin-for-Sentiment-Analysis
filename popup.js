e01fc7b16c2be3437dd62c27252eea7dd44c4453
Abhishek Manoj Sharma <abhishekmsharma@hotmail.com>
2018-05-13 10:55:19 -0700

    Adding popup.js

---
+++ popup.js
@@ -0,0 +1,40 @@
+var a=0;
+function count() {
+    a++;
+	console.log("Incrementing a");
+    document.getElementById('demo').textContent = a;
+}
+
+function receiveFromSource(string_value) {
+    console.log('Returned string:\n' + string_value);
+	document.getElementById('demo').textContent = 'hello';
+	document.getElementById('review_string').textContent = string_value;
+
+    
+}
+
+// Can we do this ? http://php.net/manual/en/reserved.variables.post.php
+
+function getReviews() {
+	chrome.tabs.sendMessage(tab.id, {text: 'get_all_reviews_string'}, receiveFromSource);
+	chrome.tabs.executeScript({
+    file: 'alert.js'
+  }); 
+}
+
+function test_function () {
+    if (0===0) {
+		console.log("Amazon product page detected");
+        chrome.tabs.sendMessage(tab.id, {text: 'get_all_reviews_string'}, receiveFromSource);
+		chrome.tabs.sendMessage(tab.id, {text: 'get_home_page_reviews'}, receiveFromSource);
+    }
+}
+
+function getURL() {
+	
+    //chrome.runtime.sendMessage({greeting: "GetURL"},receiveFromSource);
+	chrome.browserAction.onClick(tab);
+}
+
+document.getElementById('do-count').onclick = count;
+document.getElementById('get-reviews').onclick = getURL;
